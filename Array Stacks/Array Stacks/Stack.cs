c2c0de1faff563fd765d8fa9dd91664703f21e6f
Edan Saedi <edsaedi@gmail.com>
2018-11-02 17:37:48 -0700

    Added Projects

---
+++ Array Stacks/Array Stacks/Stack.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Stacks
+{
+    class Stack<T>
+    {
+        //Capacity Method
+
+        T[] stack;
+        public int Count { get; private set; }
+
+        public Stack(int capacity = 10)
+        {
+            if (capacity < 10)
+            {
+                capacity = 10;
+            }
+
+            stack = new T[capacity];
+            Count = 0;
+        }
+
+        //Add is going to add an item at the end of the array
+        public void Push(T item)
+        {
+            // if count == capacity (stack.Length)
+            if (Count == stack.Length)
+            {
+                Resize(Count * 2);
+                //call resize
+            }
+            stack[Count] = item;
+            Count++;
+        }
+
+        public T Pop()
+        {
+            //if count is 1/4 of array length, resize half array length. very optional
+
+            Count--;
+            return stack[Count];
+        }
+
+        public T Peek()
+        {
+            return stack[Count-1];
+        }
+
+        public bool IsEmpty()
+        {
+            return Count == 0;
+        }
+
+        private void Resize(int size)
+        {
+            //resize the array
+            //create a new array with size "size"
+            T[] temp = new T[size];
+            //copy everything over
+            for (int i = 0; i < stack.Length; i++)
+            {
+                temp[i] = stack[i];
+            }
+            // stack = temp
+            stack = temp;
+        }
+
+    }
+}
---
+++ Introduction to Stacks/Introduction to Stacks/Stack.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduction_to_Stacks
+{
+    public class Stack<T>
+    {
+        LinkedList<T> stackList;
+        public int Count => stackList.Count;
+
+        public Stack()
+        {
+            stackList = new LinkedList<T>();
+        }
+
+        void Push(T value)
+        {
+            stackList.AddFirst(value);
+        }
+
+        T Pop()
+        {
+            if (stackList.First == null)
+            {
+                throw new Exception("The stack is empty");
+            }
+
+            else
+            {
+                var temp = stackList.First.Value;
+                stackList.RemoveFirst();
+                return temp;
+            }
+        }
+
+        T Peek()
+        {
+            if (stackList.First == null)
+            {
+                throw new Exception("The stack is empty");
+            }
+
+            else
+            {
+                return stackList.First.Value;
+            }
+        }
+
+        bool IsEmpty()
+        {
+            if (stackList.First != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
